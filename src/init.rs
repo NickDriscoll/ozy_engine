8118e5470e3c5fbafabb168719db63b73e120dc3
Nick Driscoll <nick.a.mavros@gmail.com>
2020-02-16 00:01:32 -0500

    Init commit

---
+++ src/init.rs
@@ -0,0 +1,18 @@
+use glfw::{Glfw, Window, WindowEvent, WindowMode};
+use std::sync::mpsc::Receiver;
+
+pub fn glfw_window(window_size: (u32, u32), window_mode: WindowMode, major_version: u32, minor_version: u32, title: &str) -> (Glfw, Window, Receiver<(f64, WindowEvent)>) {
+	//Init glfw
+	let mut glfw = match glfw::init(glfw::FAIL_ON_ERRORS) {
+		Ok(g) => { g }
+		Err(e) => {	panic!("GLFW init error: {}", e); }
+	};
+
+	//Using OpenGL 3.3 core, but that could change
+	glfw.window_hint(glfw::WindowHint::ContextVersion(major_version, minor_version));
+	glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+
+	//Create window
+    let (window, events) = glfw.create_window(window_size.0, window_size.1, title, window_mode).unwrap();
+    (glfw, window, events)
+}
