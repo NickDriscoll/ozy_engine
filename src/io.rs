7781036c5eee1f7de41eb8e1731192f77f062b51
Nick Driscoll <nick.a.mavros@gmail.com>
2022-10-06 00:04:50 -0400

    Lots of DDS work

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,12 @@
 #![allow(non_camel_case_types)]
-use std::mem;
+use std::mem::{self, size_of};
 use std::fs::File;
 use std::io::{Error, Read, Write};
 use std::string::String;
 use crate::structs::*;
+
+//DDS Programming Guide
+//https://learn.microsoft.com/en-us/windows/win32/direct3ddds/dx-graphics-dds-pguide
 
 /*
 struct DDS_PIXELFORMAT {
@@ -17,16 +20,46 @@ struct DDS_PIXELFORMAT {
   DWORD dwABitMask;
 };
 */
-#[derive(Default)]
+#[derive(Debug)]
+#[repr(C)]
 pub struct DDS_PixelFormat {
-    size: u32,
-    flags: u32,
-    four_cc: u32,
-    rgb_bitcount: u32,
-    r_bitmask: u32,
-    g_bitmask: u32,
-    b_bitmask: u32,
-    a_bitmask: u32,
+    pub size: u32,
+    pub flags: u32,
+    pub four_cc: u32,
+    pub rgb_bitcount: u32,
+    pub r_bitmask: u32,
+    pub g_bitmask: u32,
+    pub b_bitmask: u32,
+    pub a_bitmask: u32,
+}
+
+impl DDS_PixelFormat {
+    pub const DDPF_ALPHAPIXELS: u32 = 0x1;
+    pub const DDPF_ALPHA: u32 = 0x2;
+    pub const DDPF_FOURCC: u32 = 0x4;
+    pub const DDPF_RGB: u32 = 0x1;
+    pub const DDPF_YUV: u32 = 0x2;
+    pub const DDPF_LUMINANCE: u32 = 0x4;
+}
+
+impl Default for DDS_PixelFormat {
+    fn default() -> Self {
+        let mut four_cc = 0u32;
+        four_cc |= ('D' as u32) << 0;
+        four_cc |= ('X' as u32) << 8;
+        four_cc |= ('1' as u32) << 16;
+        four_cc |= ('0' as u32) << 24;
+        DDS_PixelFormat {
+            size: 32,
+            flags: 0,
+            four_cc,
+            rgb_bitcount: 8,
+            r_bitmask: 0,
+            g_bitmask: 0,
+            b_bitmask: 0,
+            a_bitmask: 0,
+        }
+    }
 }
 
 /*
@@ -47,9 +80,10 @@ typedef struct {
      DWORD           dwReserved2;
 } DDS_HEADER;
    */
-#[derive(Default)]
+#[derive(Debug)]
+#[repr(C)]
 pub struct DDSHeader {
-    pub magic_word: u32,
+    pub magic_word: u32,        // 0x20534444
     pub size: u32,
     pub flags: u32,
     pub height: u32,
@@ -63,29 +97,245 @@ pub struct DDSHeader {
     pub caps2: u32,
     pub caps3: u32,
     pub caps4: u32,
-    pub reserved2: u32
+    pub reserved2: u32,
+    pub dx10_header: DDSHeader_DXT10
 }
 
 impl DDSHeader {
+    const TRUE_BC7_HEADER_SIZE: usize = 148;        //This is DDSHeader + DDSHeader_DXT10 + magic word
+    const REQUIRED_BC7_SIZE: u32 = 124;
+
+    pub const DDSD_CAPS: u32 = 0x1;
+    pub const DDSD_HEIGHT: u32 = 0x2;
+    pub const DDSD_WIDTH: u32 = 0x4;
+    pub const DDSD_PITCH: u32 = 0x8;
+    pub const DDSD_PIXELFORMAT: u32 = 0x1000;
+    pub const DDSD_MIPMAPCOUNT: u32 = 0x20000;
+    pub const DDSD_LINEARSIZE: u32 = 0x80000;
+    pub const DDSD_DEPTH: u32 = 0x800000;
+
     pub fn from_file(dds_file: &mut File) -> Self {
-        const BC7_HEADER_SIZE: usize = 148;
-        let mut header_buffer = vec![0u8; BC7_HEADER_SIZE];
+        let mut header_buffer = vec![0u8; Self::TRUE_BC7_HEADER_SIZE];
     
         dds_file.read_exact(&mut header_buffer).unwrap();
     
         let height = read_u32_from_le_bytes(&header_buffer, 12);
         let width = read_u32_from_le_bytes(&header_buffer, 16);
-        let bytes_per_scanline = read_u32_from_le_bytes(&header_buffer, 20);
+        let pitch_or_linear_size = read_u32_from_le_bytes(&header_buffer, 20);
         let mipmap_count = read_u32_from_le_bytes(&header_buffer, 28);
 
         DDSHeader {
             height,
             width,
-            pitch_or_linear_size: bytes_per_scanline,
+            pitch_or_linear_size,
             mipmap_count,
             ..Default::default()
         }
     }
+}
+
+impl Default for DDSHeader {
+    fn default() -> Self {
+        DDSHeader {
+            magic_word: 0x20534444,
+            size: Self::REQUIRED_BC7_SIZE,
+            flags: 0,
+            height: 0,
+            width: 0,
+            pitch_or_linear_size: 0,
+            depth: 0,
+            mipmap_count: 1,
+            reserved_1: [0; 11],
+            spf: DDS_PixelFormat::default(),
+            caps: 0,
+            caps2: 0,
+            caps3: 0,
+            caps4: 0,
+            reserved2: 0,
+            dx10_header: DDSHeader_DXT10 {
+                dxgi_format: DXGI_FORMAT::UNKNOWN,
+                resource_dimension: D3D10_RESOURCE_DIMENSION::TEXTURE2D,
+                misc_flag: 0,
+                array_size: 1,
+                misc_flags2: 0
+            }
+        }
+    }
+}
+
+/*
+typedef struct {
+  DXGI_FORMAT              dxgiFormat;
+  D3D10_RESOURCE_DIMENSION resourceDimension;
+  UINT                     miscFlag;
+  UINT                     arraySize;
+  UINT                     miscFlags2;
+} DDS_HEADER_DXT10;
+ */
+#[derive(Debug)]
+#[repr(C)]
+pub struct DDSHeader_DXT10 {
+    pub dxgi_format: DXGI_FORMAT,
+    pub resource_dimension: D3D10_RESOURCE_DIMENSION,
+    pub misc_flag: u32,
+    pub array_size: u32,
+    pub misc_flags2: u32
+}
+
+impl Default for DDSHeader_DXT10 {
+    fn default() -> Self {
+        DDSHeader_DXT10 {
+            dxgi_format: DXGI_FORMAT::UNKNOWN,
+            resource_dimension: D3D10_RESOURCE_DIMENSION::TEXTURE2D,
+            misc_flag: 0,
+            array_size: 1,
+            misc_flags2: 0
+        }
+    }
+}
+
+impl DDSHeader_DXT10 {
+    pub const DDS_RESOURCE_MISC_TEXTURECUBE: u32 = 0x4;
+}
+
+#[derive(Debug)]
+pub enum D3D10_RESOURCE_DIMENSION {
+    UNKNOWN = 0,
+    BUFFER = 1,
+    TEXTURE1D = 2,
+    TEXTURE2D = 3,
+    TEXTURE3D = 4
+}
+
+#[derive(Debug)]
+pub enum DXGI_FORMAT {
+    UNKNOWN = 0,
+    R32G32B32A32_TYPELESS = 1,
+    R32G32B32A32_FLOAT = 2,
+    R32G32B32A32_UINT = 3,
+    R32G32B32A32_SINT = 4,
+    R32G32B32_TYPELESS = 5,
+    R32G32B32_FLOAT = 6,
+    R32G32B32_UINT = 7,
+    R32G32B32_SINT = 8,
+    R16G16B16A16_TYPELESS = 9,
+    R16G16B16A16_FLOAT = 10,
+    R16G16B16A16_UNORM = 11,
+    R16G16B16A16_UINT = 12,
+    R16G16B16A16_SNORM = 13,
+    R16G16B16A16_SINT = 14,
+    R32G32_TYPELESS = 15,
+    R32G32_FLOAT = 16,
+    R32G32_UINT = 17,
+    R32G32_SINT = 18,
+    R32G8X24_TYPELESS = 19,
+    D32_FLOAT_S8X24_UINT = 20,
+    R32_FLOAT_X8X24_TYPELESS = 21,
+    X32_TYPELESS_G8X24_UINT = 22,
+    R10G10B10A2_TYPELESS = 23,
+    R10G10B10A2_UNORM = 24,
+    R10G10B10A2_UINT = 25,
+    R11G11B10_FLOAT = 26,
+    R8G8B8A8_TYPELESS = 27,
+    R8G8B8A8_UNORM = 28,
+    R8G8B8A8_UNORM_SRGB = 29,
+    R8G8B8A8_UINT = 30,
+    R8G8B8A8_SNORM = 31,
+    R8G8B8A8_SINT = 32,
+    R16G16_TYPELESS = 33,
+    R16G16_FLOAT = 34,
+    R16G16_UNORM = 35,
+    R16G16_UINT = 36,
+    R16G16_SNORM = 37,
+    R16G16_SINT = 38,
+    R32_TYPELESS = 39,
+    D32_FLOAT = 40,
+    R32_FLOAT = 41,
+    R32_UINT = 42,
+    R32_SINT = 43,
+    R24G8_TYPELESS = 44,
+    D24_UNORM_S8_UINT = 45,
+    R24_UNORM_X8_TYPELESS = 46,
+    X24_TYPELESS_G8_UINT = 47,
+    R8G8_TYPELESS = 48,
+    R8G8_UNORM = 49,
+    R8G8_UINT = 50,
+    R8G8_SNORM = 51,
+    R8G8_SINT = 52,
+    R16_TYPELESS = 53,
+    R16_FLOAT = 54,
+    D16_UNORM = 55,
+    R16_UNORM = 56,
+    R16_UINT = 57,
+    R16_SNORM = 58,
+    R16_SINT = 59,
+    R8_TYPELESS = 60,
+    R8_UNORM = 61,
+    R8_UINT = 62,
+    R8_SNORM = 63,
+    R8_SINT = 64,
+    A8_UNORM = 65,
+    R1_UNORM = 66,
+    R9G9B9E5_SHAREDEXP = 67,
+    R8G8_B8G8_UNORM = 68,
+    G8R8_G8B8_UNORM = 69,
+    BC1_TYPELESS = 70,
+    BC1_UNORM = 71,
+    BC1_UNORM_SRGB = 72,
+    BC2_TYPELESS = 73,
+    BC2_UNORM = 74,
+    BC2_UNORM_SRGB = 75,
+    BC3_TYPELESS = 76,
+    BC3_UNORM = 77,
+    BC3_UNORM_SRGB = 78,
+    BC4_TYPELESS = 79,
+    BC4_UNORM = 80,
+    BC4_SNORM = 81,
+    BC5_TYPELESS = 82,
+    BC5_UNORM = 83,
+    BC5_SNORM = 84,
+    B5G6R5_UNORM = 85,
+    B5G5R5A1_UNORM = 86,
+    B8G8R8A8_UNORM = 87,
+    B8G8R8X8_UNORM = 88,
+    R10G10B10_XR_BIAS_A2_UNORM = 89,
+    B8G8R8A8_TYPELESS = 90,
+    B8G8R8A8_UNORM_SRGB = 91,
+    B8G8R8X8_TYPELESS = 92,
+    B8G8R8X8_UNORM_SRGB = 93,
+    BC6H_TYPELESS = 94,
+    BC6H_UF16 = 95,
+    BC6H_SF16 = 96,
+    BC7_TYPELESS = 97,
+    BC7_UNORM = 98,
+    BC7_UNORM_SRGB = 99,
+    AYUV = 100,
+    Y410 = 101,
+    Y416 = 102,
+    NV12 = 103,
+    P010 = 104,
+    P016 = 105,
+    //420_OPAQUE = 106,
+    YUY2 = 107,
+    Y210 = 108,
+    Y216 = 109,
+    NV11 = 110,
+    AI44 = 111,
+    IA44 = 112,
+    P8 = 113,
+    A8P8 = 114,
+    B4G4R4A4_UNORM = 115,
+    P208 = 130,
+    V208 = 131,
+    V408 = 132,
+    SAMPLER_FEEDBACK_MIN_MIP_OPAQUE,
+    SAMPLER_FEEDBACK_MIP_REGION_USED_OPAQUE,
+    FORCE_UINT = 0xffffffff
+}
+
+pub fn compute_pitch_bc(width: u32, block_size: u32) -> u32 {
+    //max( 1, ((width+3)/4) ) * block-size
+    u32::max(1, (width + 3) / 4) * block_size
 }
 
 pub enum IndexType {
@@ -412,16 +662,17 @@ pub fn read_pascal_strings(file: &mut File, count: usize) -> Result<Vec<String>,
 	Ok(strings)
 }
 
-pub fn write_pascal_strings(file: &mut File, strs: &[&str]) {
+pub fn write_pascal_strings(file: &mut File, strs: &[&str]) -> std::io::Result<()> {
     for i in 0..strs.len() {
         let s = strs[i];
         let l = s.len() as u32;
         if let Err(e) = file.write(&u32::to_le_bytes(l)) {
-            panic!("Couldn't write pascal strings: {}", e);
+            return Err(e)
         }        
 
         if let Err(e) = file.write(s.as_bytes()) {
-            panic!("Couldn't write pascal strings: {}", e);
+            return Err(e)
         }
     }
+    Ok(())
 }
